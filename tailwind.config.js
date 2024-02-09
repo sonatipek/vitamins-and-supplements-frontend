02b40d3e6f0d4236d08c6c7211e032d565c5890f
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-02-10 02:27:02 +0300

    project colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#EF4444",
+        darkBrand: "#991B1B",
+        darkest: "#18181B",
+        dark: "#3B3B43",
+        mid: "#858594",
+        light: "#D4D4D8",
+        lightest: "#fff",
+      },
+    },
   },
   plugins: [],
-}
+};
