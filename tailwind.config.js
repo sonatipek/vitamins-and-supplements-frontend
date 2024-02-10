35a9a54bb87f75ff6e609847de61d4d6cd887c6c
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-02-10 03:21:41 +0300

    tailwindcss container padding configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,11 @@ export default {
     container: {
       center: true,
       padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "5rem",
-        "2xl": "6rem",
+        DEFAULT: "0.5rem",
+        sm: "0.75rem",
+        lg: "1rem",
+        xl: "2rem",
+        "2xl": "4rem",
       },
     },
     extend: {
