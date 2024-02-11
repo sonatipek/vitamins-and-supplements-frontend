a10e4de933d87c3f45238e2202d70f43ce12a990
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-02-11 15:55:33 +0300

    temporary logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Github, Moon } from "lucide-react";
+import BrandLogo from "../assets/brand_logo.svg";
 
 export default function Navbar() {
   const navLinks = [
@@ -8,9 +9,12 @@ export default function Navbar() {
   return (
     <header>
       <nav className="container py-3 flex items-center justify-between">
-        <h1 className="inline-block w-1/2 md:w-1/5 text-2xl text-darkest font-semibold">
-          Vitamins & Supplements
-        </h1>
+        <a href="/">
+          <img src={BrandLogo} alt="Vitamins and Supplements Brand Logo" width={88} />
+          <h1 className="hidden">
+            Vitamins & Supplements
+          </h1>
+        </a>
 
         <ul className="flex gap-5 *:font-medium *:text-darkest">
           {navLinks.map((value, index) => (
@@ -26,11 +30,11 @@ export default function Navbar() {
             href="https://github.com/sonatipek/vitamins-and-supplements-frontend"
             className="flex px-5 gap-2 text-dark border-r-2 border-light hover:underline"
           >
-            <Github strokeWidth={1.5}/> Repo
+            <Github strokeWidth={1.5} /> Repo
           </a>
 
           <button className="pl-2 text-dark hover:scale-105  transition-transform duration-300">
-            <Moon strokeWidth={1.5}/>
+            <Moon strokeWidth={1.5} />
           </button>
         </div>
       </nav>
