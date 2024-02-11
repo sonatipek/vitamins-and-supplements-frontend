746b881f056c068ab2d2460d9a3120a60c93a3a6
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-02-11 16:39:46 +0300

    footer added

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Navbar from "./components/Navbar"
+import Footer from './components/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <App />
+    <Footer />
   </React.StrictMode>,
 )
