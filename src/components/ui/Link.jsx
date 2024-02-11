7176e688d6bfbe189115536565e7d7805942ec34
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-02-11 18:39:39 +0300

    Link and Button components hover states added

--- src/components/ui/Link.jsx
+++ src/components/ui/Link.jsx
@@ -3,13 +3,13 @@ export default function Link({ children, href, classname, icon, type, target }) 
       <a
         href={href}
         target={!target ? "_self" : target}
-        className={`inline-flex justify-center items-center ${
+        className={`inline-flex justify-center items-center group ${
           type === "button"
-            ? "bg-brand px-14 py-3.5 rounded-full flex items-center justify-center text-white"
-            : "text-mid underline underline-offset-2 hover:text-dark dark:hover:text-light"
+            ? "bg-brand border border-brand hover:bg-transparent hover:text-darkest hover:dark:text-white transition-colors duration-300 px-14 py-3.5 rounded-full flex items-center justify-center text-white font-medium shadow-lg"
+            : "text-mid underline underline-offset-4 hover:text-dark dark:hover:text-light"
         } ${classname} ${!icon ? "mr-0" : "mr-1"}`}
       >
-        {!icon ? <></> : <span className="mr-1">{icon}</span>}
+        {!icon ? <></> : <span className="mr-1 group-hover:-translate-y-1 transition-transform duration-200">{icon}</span>}
         {children}
       </a>
     );
