a52a7be5d28607cd217e020c739a98970dc4747e
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-02-11 18:53:54 +0300

    set text and title line height properties

--- src/components/ui/Text.jsx
+++ src/components/ui/Text.jsx
@@ -1,10 +1,10 @@
 export default function Text({ children, type, className }) {
     return (
       <p
-        className={` ${
+        className={`line-he ${
           type === "subtext"
-            ? "text-sm text-mid dark:text-mid"
-            : "text-base text-dark dark:text-light"
+            ? "text-sm text-mid dark:text-mid leading-normal"
+            : "text-base text-dark dark:text-light leading-relaxed"
         } ${className}`}
       >
         {children}
--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -2,7 +2,7 @@ export default function Title({ children, size, className }) {
     return (
       <h1
         className={`font-light ${
-          size === "lg" ? "text-5xl" : size === "md" ? "text-3xl" : "text-xl"
+          size === "lg" ? "text-5xl leading-relaxed" : size === "md" ? "text-3xl leading-normal" : "text-xl leading-snug"
         }  text-darkest dark:text-white ${className}`}
       >
         {children}
