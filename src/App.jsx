b0539ee9497e0f7e12b632c39eed01e6439a00ff
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-02-12 23:46:58 +0300

    'Kimlere Yönelik' section and List section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Paragraph from "./components/Paragraph";
+import List from "./components/ui/List";
 
 function App() {
   return (
@@ -15,8 +16,14 @@ function App() {
         title="Neden Kullanmalısın ?"
         description="Sağlıklı bir yaşam tarzı sürdürmek ve zindelikten maksimum düzeyde faydalanmak için doğru beslenmemiz gerekir. Bunun için de günlük aktivitenize veya spor alışkanlıklarınıza göre ek takviyelere ihtiyaç duyabiliriz. Ancak bu ek takviyelere ne kadar ihtiyacınız var? Bu ek takviyeler sizin durumunuz için gerçekten faydalı mı? İşte tam bu noktada, sizin için ihtiyacınız olabilecek supplementleri, birbirini tekrar eden kopya içerikli “pazarlama içerikleri”nden, kâr amacı ile takviyeyi öven içeriklerden uzak bir şekilde kendiniz keşfetmenize olanak tanımak istiyoruz. "
       />
-      
+
       <Features />
+
+      <Paragraph
+        title="Kimlere Yönelik"
+        description="Başlıca aşağıdaki grubu içerse de tek seferlik takviye kullanmak zorunda kalan, bir probleme çözüm arayan herkese yöneliktir."
+      />
+      <List listElements={["Sporcular", "Beslenme ve takviyeler hakkında bilgi edinmek isteyenler", "Sağlık bilinci yüksek bireyler", "Dengeli beslenme alışkanlıklarını geliştirmek isteyenler"]}/>
     </>
   );
 }
