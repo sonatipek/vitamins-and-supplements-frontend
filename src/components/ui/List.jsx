a44161c2db8b3948c4309b27d47c1b8c9d1dcfce
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-02-12 23:45:22 +0300

    List component created

---
+++ src/components/ui/List.jsx
@@ -0,0 +1,12 @@
+export default function List({ listElements }) {
+  return (
+    <ul className="container lg:px-28 mt-3 grid grid-cols-2 gap-y-3">
+      {listElements.map((value, index) => (
+        <li key={index} className="flex items-center justify-start text-darkest">
+          <span className="size-4 bg-brand rounded-full mr-3"></span>
+          {value}
+        </li>
+      ))}
+    </ul>
+  );
+}
