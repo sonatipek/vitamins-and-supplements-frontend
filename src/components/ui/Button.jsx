6014a361de1e2137f5a1101e792631445ae5cbd4
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-02-12 23:10:19 +0300

    changed the vertical padding of the button view

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, className, onClick, icon }) {
   return (
     <button
-      className={`bg-brand border border-brand hover:bg-transparent hover:text-darkest hover:dark:text-white transition-colors duration-300 px-14 py-3.5 rounded-full inline-flex items-center justify-center text-white font-medium group ${className}`}
+      className={`bg-brand border border-brand hover:bg-transparent hover:text-darkest hover:dark:text-white transition-colors duration-300 px-14 py-2.5 rounded-full inline-flex items-center justify-center text-white font-medium group ${className}`}
       onClick={onClick}
     >
       {!icon ? (
--- src/components/ui/Link.jsx
+++ src/components/ui/Link.jsx
@@ -5,7 +5,7 @@ export default function Link({ children, href, classname, icon, type, target }) 
         target={!target ? "_self" : target}
         className={`inline-flex justify-center items-center group ${
           type === "button"
-            ? "bg-brand border border-brand hover:bg-transparent hover:text-darkest hover:dark:text-white transition-colors duration-300 px-14 py-3.5 rounded-full flex items-center justify-center text-white font-medium shadow-lg"
+            ? "bg-brand border border-brand hover:bg-transparent hover:text-darkest hover:dark:text-white transition-colors duration-300 px-14 py-2.5 rounded-full flex items-center justify-center text-white font-medium shadow-lg"
             : "text-mid underline underline-offset-4 hover:text-dark dark:hover:text-light"
         } ${classname} ${!icon ? "mr-0" : "mr-1"}`}
       >
