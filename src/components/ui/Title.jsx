9b9263bfe6b1415e655d02ec3e4d9211f5791dfc
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-03-28 14:52:57 +0300

    custom 404 page added

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -1,7 +1,7 @@
 export default function Title({ children, size, className }) {
     return (
       <h1
-        className={`font-light ${
+        className={`font-light mb-2 ${
           size === "lg" ? "text-4xl md:text-5xl leading-relaxed" : size === "md" ? "text-2xl md:text-3xl leading-normal" : "text-lg md:text-xl leading-snug"
         }  text-darkest dark:text-white ${className}`}
       >
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,32 @@
 import { useRouteError } from "react-router-dom";
+import NotFound from "../assets/not_found.svg";
+import Title from "../components/ui/Title";
+import Link from "../components/ui/Link";
+import { HomeIcon } from "lucide-react";
 
 export default function ErrorPage() {
-    const error = useRouteError();
-  
-    return (
-      <div id="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <b>{error.status}</b>
-          <i> {error.statusText || error.message}</i>
-        </p>
-      </div>
-    );
+  const error = useRouteError();
+
+  return (
+    <>
+      {error.status === 404 ? (
+        <div className="flex flex-col items-center justify-center">
+          <Title className="font-semibold text-center mt-12" size="lg">Sanırım kayboldun! Aramak istediğin şeyi bulamadık.</Title>
+          <img src={NotFound} alt="not found page" className="w-100 sm:w-3/4 md:w-2/4 mx-auto mt-12" />
+         
+          <Link href="/" icon={<HomeIcon />} type="button" classname="cursor-pointer mt-12">Ana Sayfaya Dön</Link>
+    
+        </div>
+      ) : (
+        <div id="error-page">
+          <h1>Oops!</h1>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p>
+            <b>{error.status}</b>
+            <i> {error.statusText || error.message}</i>
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
