0283137445a1c57b9d05e1f9534da85fd6e99057
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-03-31 09:38:02 +0300

    features comp. new design

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,9 @@
 import { Users, Info, ShieldCheck } from "lucide-react";
 
 import Card from "./ui/Card";
-import Link from "./ui/Link";
 import Illustration from "../assets/feature_illustration.svg";
+import Title from "./ui/Title";
+import Text from "./ui/Text";
 
 const featuresList = [
   {
@@ -24,30 +25,47 @@ const featuresList = [
 
 export default function Features() {
   return (
-    <section className="bg-gradient-to-r from-brand to-darkBrand mt-6 relative pt-14 md:pt-28 pb-7 md:pb-14 overflow-hidden">
+    <section className="relative pt-14 md:pt-28 pb-7 md:pb-14 overflow-hidden">
       <img
         src={Illustration}
         className="hidden xl:inline absolute right-0 xl:-right-10 2xl:right-0 bottom-0"
         alt="illustration of a man holding an apple"
       />
 
-      <div className="container flex flex-col items-center">
-        <div className="grid md:grid-cols-3 gap-2 md:gap-4 lg:gap-6">
+      <div className="container flex flex-col items-center text-center">
+        <Title className="!font-bold text-transparent bg-clip-text bg-gradient-to-r from-brand to-darkBrand">
+          Amacımız ne?
+        </Title>
+        <Title size="lg" className="!font-bold !text-dark">
+          Gerçekten İhtiyaç Mı?
+        </Title>
+        <Text className="text-mid text-xl mt-2 px-48">
+          Bazen “Bu takviyeye gerçekten ihtiyacım var mı?” sorusunun cevabını
+          ararken uzun kaynaklar arasında kaybolabilir, pazarlamacıların kurbanı
+          olabiliriz veya tam olarak ihtiyacımız olan takviyeyi bulmak zor
+          olabilir. Bu tür ihtiyaçlar sırasında gerekli araştırmanın zaman
+          maliyetini düşürmek ve kullanıcıların sağlıklı yaşam alışkanlıklarını
+          daha kolay oluşturmasını sağlamak amacıyla oluşturulmuştur.
+        </Text>
+
+        <Title className="!font-bold text-transparent bg-clip-text bg-gradient-to-r from-brand to-darkBrand mt-14 mb-2">
+          Takviyelere ne kadar ihtiyacınız var?
+        </Title>
+        <Text className="text-mid px-48">
+          Günlük aktivitemize veya spor alışkanlıklarımıza göre takviye gıdalara
+          ihtiyaç duyabiliriz. Ancak takviyeler sizin durumunuz için gerçekten
+          faydalı mı? İşte tam bu noktada, sizin için ihtiyacınız olabilecek
+          supplementleri, birbirini tekrar eden kopya içerikli “pazarlama
+          içerikleri”nden, kâr amacı ile takviyeyi öven içeriklerden uzak bir
+          şekilde kendiniz keşfetmenize olanak tanımak istiyoruz.
+        </Text>
+        <div className="grid md:grid-cols-3 gap-2 md:gap-4 lg:gap-6 px-24">
           {featuresList.map((value, index) => (
             <Card title={value.title} icon={value.icon} key={index}>
               {value.desc}
             </Card>
           ))}
         </div>
-
-        <Link
-          type="button"
-          href="/supplements"
-          classname="!shadow-none !py-2 mt-7 md:mt-14 max-w-fit"
-          color="white"
-        >
-          Hemen Keşfet
-        </Link>
       </div>
     </section>
   );
